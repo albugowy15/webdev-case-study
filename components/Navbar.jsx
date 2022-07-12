c30eec8e0eebe503ecca0d55da84c6914df35df2
albugowy15 <muhammadbughowy@gmail.com>
2022-07-12 08:49:56 +0700

    add navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import { FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  return (
+    <>
+      <div className="flex items-center justify-between py-3">
+        <div className="font-bold text-xl">
+          <Link href="/">Bughowi</Link>
+        </div>
+
+        {/* Navbar on large screen */}
+        <ul className="hidden sm:flex items-center justify-between gap-2">
+          <li>
+            <a href="#biodata">Biodata</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#education">Education</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        {/* End Here*/}
+
+        {/* Navbar on small screen / mobile device */}
+        <div className="block sm:hidden cursor-pointer">
+          <FaBars onClick={() => setNav(!nav)} />
+        </div>
+        {/* End here */}
+      </div>
+
+      {/* Show navar in small screen */}
+      <div
+        className={
+          nav
+            ? "fixed bg-black bg-opacity-60 h-screen m-screen z-50 top-0 left-0 right-0"
+            : "hidden"
+        }
+      >
+        <div className="w-[80%] h-full bg-gray-800 text-slate-200 p-5">
+          <div className="flex items-center justify-between">
+            <h1 className="font-bold text-2xl ">Bughowi</h1>
+            <button
+              onClick={() => setNav(!nav)}
+              className="p-4 rounded-full bg-slate-900 shadow-lg"
+            >
+              <IoClose />
+            </button>
+          </div>
+          <div className="flex flex-col items-start justify-start gap-5 my-4 text-lg">
+            <a href="#biodata">Biodata</a>
+            <a href="#skills">Skills</a>
+            <a href="#education">Education</a>
+            <a href="#contact">Contact</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import Navbar from "../components/Navbar";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="container px-2 mx-auto">
+      <Navbar />
+    </div>
+  );
 }
