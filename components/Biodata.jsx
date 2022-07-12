6336377db25bda342a16ec6dccd9e257ae848b88
albugowy15 <muhammadbughowy@gmail.com>
2022-07-12 16:15:21 +0700

    modify biodata component

--- components/Biodata.jsx
+++ components/Biodata.jsx
@@ -3,8 +3,11 @@ import Image from "next/image";
 
 const Biodata = () => {
   return (
-    <div id="biodata" className="flex flex-col md:flex-row items-center my-5">
-      <div className="w-[300px] md:w-full h-[300px] lg:h-[400px] relative border-8 border-gray-700 mx-auto">
+    <div
+      id="biodata"
+      className="flex flex-col md:flex-row items-center justify-center my-5"
+    >
+      <div className="w-[300px] md:w-full lg:w-[600px] h-[300px] lg:h-[400px] relative border-8 border-gray-700 mx-auto">
         <Image
           src="/profil.jpg"
           layout="fill"
@@ -12,7 +15,7 @@ const Biodata = () => {
           alt="profil image"
         ></Image>
       </div>
-      <div className="p-4 flex flex-col items-start justify-start gap-4">
+      <div className="p-4 flex flex-col items-start justify-start gap-4 md:gap-10">
         <h1 className="font-bold text-3xl">Mohamad Kholid Bughowi</h1>
         <p>
           An organized and self motivated human with the vision to collaborate
@@ -20,6 +23,9 @@ const Biodata = () => {
           make decisions and to deal with the challenges to reach his goals and
           objectives.
         </p>
+        <button className="py-2 px-2 bg-orange-600 text-bold rounded-md">
+          Download CV
+        </button>
       </div>
     </div>
   );
